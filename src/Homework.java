63e6dc2f861a512291c2b900ca3bf1f55dab12ab
saromanukyan <81385397+saromanukyan@users.noreply.github.com>
2021-10-15 21:04:57 +0400

    update

--- src/Homework.java
+++ src/Homework.java
@@ -2,7 +2,7 @@ class operations {
     public static void main(String[] args) {
         int a = 13;
         int b = 5;
-
+System.out.println(a == b);System.out.println(a == b);
         System.out.println(a == b);
         System.out.println(a != b);
         System.out.println(a + b);
