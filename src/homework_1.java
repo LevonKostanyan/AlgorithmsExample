a6cecf6dde808b2c225933e23356a2dd9d5fd981
Kost333 <levonkostanyan2021@mail.ru>
2022-09-09 15:24:36 +0400

    change

--- src/homework_1.java
+++ src/homework_1.java
@@ -1,100 +1,100 @@
 public class homework_1 {
     public static void main(String[] args) {
 
-//1.
-//        arithmetic օperators(թվաբանական);
+// 1.
+//       arithmetic operators(թվաբանական);
+//
+//       + addition
+//       - subtraction
+//       *	Multiplication
+//       /	Division
+//       %	Modulus
+//       ++ Increment
+//       -- Decrement
+//
+//       relational operators(համեմատության);
+//
+//       == Equal to
+//       != Not equal
+//       >	Greater than
+//       <	Less than
+//       >= Greater than or equal to
+//       <= Less than or equal to
+//
+//       logical operators(տրամաբանական);
+//
+//       and && Returns true if both statements are true
+//       or || Returns true if one of the statements is true
+//       not ! Reverse the result, returns false if the result is true
+//
+//       bitwise operators(բիթային);
+//
+//       Bitwise OR (|)
+//       Bitwise AND (&)
+//       Bitwise XOR (^)
+//
+//       assignment operators(վերագրման);
+//
+//       =
+//       +=
+//       -=
+//       *=
+//       /=
+//       %=
 
-//        + addition
-//        - subtraction
-//        *	Multiplication
-//        /	Division
-//        %	Modulus
-//        ++ Increment
-//        -- Decrement
+// 2.
+       int a = 15;
+       if(a % 3 == 0 && a % 5 == 0){
+           System.out.println("բաժանվում է");
+       } else {
+           System.out.println("չի բաժանվում");
+       }
 
-//        relational օperators(համեմատության);
+// 3․
+       int x = 1, y = 1, z = 1;
+       if(x < y && x < z){
+           System.out.println(x);
+       } else if (y < x && y < z){
+           System.out.println(y);
+       } else if (z < x && z < y) {
+           System.out.println(z);
+       } else {
+           System.out.println("փոքրագույն թիվ չկա ");
+       }
 
-//        == Equal to
-//        != Not equal
-//        >	Greater than
-//        <	Less than
-//        >= Greater than or equal to
-//        <= Less than or equal to
+// 4.
+       int a = 6, b = 5;
+       int result = 2;
+       result = (a > b) ? result * (a - b) : (a < b) ? result * (a + b) : result;
+       System.out.println(result);
 
-//        logical օperators(տրամաբանական);
-
-//        and && Returns true if both statements are true
-//        or || Returns true if one of the statements is true
-//        not ! Reverse the result, returns false if the result is true
-
-//        bitwise օperators(բիթային);
-
-//        Bitwise OR (|)
-//        Bitwise AND (&)
-//        Bitwise XOR (^)
-
-//        assignment օperators(վերագրման);
-
-//        =
-//        +=
-//        -=
-//        *=
-//        /=
-//        %=
-
-//2.
-//        int a = 15;
-//        if(a % 3 == 0 && a % 5 == 0){
-//            System.out.println("բաժանվում է");
-//        } else {
-//            System.out.println("չի բաժանվում");
-//        }
-
-//3․
-//        int x = 1, y = 1, z = 1;
-//        if(x < y && x < z){
-//            System.out.println(x);
-//        } else if (y < x && y < z){
-//            System.out.println(y);
-//        } else if (z < x && z < y) {
-//            System.out.println(z);
-//        } else {
-//            System.out.println("poqraguyn tiv chka");
-//        }
-
-//4.
-//        int a = 6, b = 5;
-//        int result = 2;
-//        result = (a > b) ? result * (a - b) : (a < b) ? result * (a + b) : result;
-//        System.out.println(result);
-
-//5.
-//        int day = 8;
-//        switch (day) {
-//            case 1:
-//                System.out.println("Erkushabti");
-//                break;
-//            case 2:
-//                System.out.println("Ereqshabti");
-//                break;
-//            case 3:
-//                System.out.println("Chorqeqshabti");
-//                break;
-//            case 4:
-//                System.out.println("Hingshabti");
-//                break;
-//            case 5:
-//                System.out.println("Urbat");
-//                break;
-//            case 6:
-//                System.out.println("Shabat");
-//                break;
-//            case 7:
-//                System.out.println("Kiraki");
-//                break;
-//            default:
-//                System.out.println("Գոյություն չունի");
-//        }
-//6
+// 5.
+       int day = 8;
+       switch (day) {
+           case 1:
+               System.out.println("Monday");
+               break;
+           case 2:
+               System.out.println("Tuesday");
+               break;
+           case 3:
+               System.out.println("Wednesday");
+               break;
+           case 4:
+               System.out.println("Thursday");
+               break;
+           case 5:
+               System.out.println("Friday");
+               break;
+           case 6:
+               System.out.println("Saturday");
+               break;
+           case 7:
+               System.out.println("Sunday");
+               break;
+           default:
+               System.out.println("Does not exist");
+       }
+// 6
     }
 }
--- src/homework_2.java
+++ src/homework_2.java
@@ -3,52 +3,52 @@ public class homework_2 {
 
 //    6.
 
-//        int n = 100;
-//        for(int i = 0; i <= n;i++ ) {
-//            if(i % 3 == 0 && i % 5 == 0){
-//                System.out.println(i);
-//            }
-//        }
+        int n = 100;
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
 
 //    7.
 
-//        int n = 4568;
-//        int lastDigit = 0;
-//        int sum = 0;
-//        if(n >= 1000 && n <= 9999) {
-//            while (n > 0) {
-//                lastDigit = n % 10;
-//                sum = sum + lastDigit;
-//                n = (n - lastDigit) / 10;
-//            }
-//            if(sum % 2 == 0) {
-//                System.out.println("Evenish");
-//            } else {
-//                System.out.println("Oddish");
-//            }
-//        } else {
-//            System.out.println("tivy petq e lini qaranish");
-//        }
+        int n = 4568;
+        int lastDigit = 0;
+        int sum = 0;
+        if (n >= 1000 && n <= 9999) {
+            while (n > 0) {
+                lastDigit = n % 10;
+                sum = sum + lastDigit;
+                n = (n - lastDigit) / 10;
+            }
+            if (sum % 2 == 0) {
+                System.out.println("Even_ish");
+            } else {
+                System.out.println("Odd_ish");
+            }
+        } else {
+            System.out.println("Number must be four digits");
+        }
 
 //     8.
 
-//        int n = -10;
-//        if(n > 0) {
-//            for (int i = n; i >= 0; i--) {
-//                System.out.println(i);
-//            }
-//        } else {
-//            for (int i = n; i <= 0; i++) {
-//                System.out.println(i);
-//            }
-//        }
+        int n = -10;
+        if (n > 0) {
+            for (int i = n; i >= 0; i--) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = n; i <= 0; i++) {
+                System.out.println(i);
+            }
+        }
 
 //     9.
 
-//        int n = 7;
-//        for(int i = 0; i <= 10; i++){
-//            System.out.println(n + " * " + i + " = " + n * i);
-//        }
+        int n = 7;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
 
     }
 }
